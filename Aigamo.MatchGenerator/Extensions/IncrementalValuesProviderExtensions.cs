215eee91a8087a23c0a1498f8d7adf9bb4ea5940
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2026-04-01 19:43:46 +0900

    Rename EnumerableExtensions to IncrementalValuesProviderExtensions

--- Aigamo.MatchGenerator/Extensions/IncrementalValuesProviderExtensions.cs
+++ Aigamo.MatchGenerator/Extensions/IncrementalValuesProviderExtensions.cs
@@ -2,7 +2,7 @@ using Microsoft.CodeAnalysis;
 
 namespace Aigamo.MatchGenerator.Extensions;
 
-internal static class EnumerableExtensions
+internal static class IncrementalValuesProviderExtensions
 {
 	// https://github.com/dotnet/roslyn/blob/8f24ed69cbbf377573c403d6c8febbc92b560343/src/Compilers/Core/Portable/InternalUtilities/EnumerableExtensions.cs#L287
 	public static IncrementalValuesProvider<T> WhereNotNull<T>(this IncrementalValuesProvider<T?> source)
